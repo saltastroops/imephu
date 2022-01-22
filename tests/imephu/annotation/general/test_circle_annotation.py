612418ea6243911f155fdc82557e0761a24f6f12
Christian Hettlage <hettlage@saao.ac.za>
2022-01-22 13:25:34 +0200

    Fixed linting issues

--- tests/imephu/annotation/general/test_circle_annotation.py
+++ tests/imephu/annotation/general/test_circle_annotation.py
@@ -29,11 +29,30 @@ def test_circle_annotation(fits_file, fits_center, check_finder):
 def test_circle_annotation_rotated(pivot, angle, fits_file, check_finder):
     """Test rotated circle annotations."""
     finder_chart = FinderChart(fits_file)
-    circle_annotation = CircleAnnotation(SkyCoord(ra="00h40m00s", dec="-60d00m00s"), 15 * u.arcsec, wcs=finder_chart.wcs, edgecolor="none", facecolor="gray")
+    circle_annotation = CircleAnnotation(
+        SkyCoord(ra="00h40m00s", dec="-60d00m00s"),
+        15 * u.arcsec,
+        wcs=finder_chart.wcs,
+        edgecolor="none",
+        facecolor="gray",
+    )
     rotated_circle_annotation = circle_annotation.rotate(pivot, angle)
     rotated_circle_annotation._kwargs["color"] = "blue"
-    legend = TextAnnotation(SkyCoord(ra="00h40m36s", dec="-59d55m30s"), f"Rotated by {angle.to_value(u.deg)} deg", wcs=finder_chart.wcs, color="blue", horizontalalignment="left")
-    pivot_marker = CircleAnnotation(pivot, 12 * u.arcsec, wcs=finder_chart.wcs, edgecolor="none", facecolor="orange", alpha=0.7)
+    legend = TextAnnotation(
+        SkyCoord(ra="00h40m36s", dec="-59d55m30s"),
+        f"Rotated by {angle.to_value(u.deg)} deg",
+        wcs=finder_chart.wcs,
+        color="blue",
+        horizontalalignment="left",
+    )
+    pivot_marker = CircleAnnotation(
+        pivot,
+        12 * u.arcsec,
+        wcs=finder_chart.wcs,
+        edgecolor="none",
+        facecolor="orange",
+        alpha=0.7,
+    )
     finder_chart.add_annotation(pivot_marker)
     finder_chart.add_annotation(circle_annotation)
     finder_chart.add_annotation(rotated_circle_annotation)
@@ -42,16 +61,27 @@ def test_circle_annotation_rotated(pivot, angle, fits_file, check_finder):
 
 
 @pytest.mark.parametrize("displacement", [(0, 0) * u.arcmin, (2.5, -4) * u.arcmin])
-def test_circle_annotation_translated(displacement, fits_file, fits_center, check_finder):
+def test_circle_annotation_translated(
+    displacement, fits_file, fits_center, check_finder
+):
     """Test translated circle annotations."""
-    s = f"Some text"
     finder_chart = FinderChart(fits_file)
     circle_annotation = CircleAnnotation(
-        fits_center, 15 * u.arcsec, wcs=finder_chart.wcs, edgecolor="none", facecolor="gray"
+        fits_center,
+        15 * u.arcsec,
+        wcs=finder_chart.wcs,
+        edgecolor="none",
+        facecolor="gray",
     )
     translated_circle_annotation = circle_annotation.translate(displacement)
     translated_circle_annotation._kwargs["color"] = "blue"
-    legend = TextAnnotation(SkyCoord(ra="00h40m36s", dec="-59d55m30s"), f"Translated by {displacement.to_value(u.arcmin)} arcmin", wcs=finder_chart.wcs, color="blue", horizontalalignment="left")
+    legend = TextAnnotation(
+        SkyCoord(ra="00h40m36s", dec="-59d55m30s"),
+        f"Translated by {displacement.to_value(u.arcmin)} arcmin",
+        wcs=finder_chart.wcs,
+        color="blue",
+        horizontalalignment="left",
+    )
     finder_chart.add_annotation(circle_annotation)
     finder_chart.add_annotation(translated_circle_annotation)
     finder_chart.add_annotation(legend)
--- tests/imephu/annotation/general/test_text_annotation.py
+++ tests/imephu/annotation/general/test_text_annotation.py
@@ -31,11 +31,29 @@ def test_text_annotation(fits_file, fits_center, check_finder):
 def test_text_annotation_rotated(pivot, angle, fits_file, check_finder):
     """Test rotated text annotations."""
     finder_chart = FinderChart(fits_file)
-    text_annotation = TextAnnotation(SkyCoord(ra="00h40m00s", dec="-60d00m00s"), "Some text", wcs=finder_chart.wcs, color="gray")
+    text_annotation = TextAnnotation(
+        SkyCoord(ra="00h40m00s", dec="-60d00m00s"),
+        "Some text",
+        wcs=finder_chart.wcs,
+        color="gray",
+    )
     rotated_text_annotation = text_annotation.rotate(pivot, angle)
     rotated_text_annotation._kwargs["color"] = "blue"
-    legend = TextAnnotation(SkyCoord(ra="00h40m36s", dec="-59d55m30s"), f"Rotated by {angle.to_value(u.deg)} deg", wcs=finder_chart.wcs, color="blue", horizontalalignment="left")
-    pivot_marker = CircleAnnotation(pivot, 12 * u.arcsec, wcs=finder_chart.wcs, edgecolor="none", facecolor="orange", alpha=0.7)
+    legend = TextAnnotation(
+        SkyCoord(ra="00h40m36s", dec="-59d55m30s"),
+        f"Rotated by {angle.to_value(u.deg)} deg",
+        wcs=finder_chart.wcs,
+        color="blue",
+        horizontalalignment="left",
+    )
+    pivot_marker = CircleAnnotation(
+        pivot,
+        12 * u.arcsec,
+        wcs=finder_chart.wcs,
+        edgecolor="none",
+        facecolor="orange",
+        alpha=0.7,
+    )
     finder_chart.add_annotation(pivot_marker)
     finder_chart.add_annotation(text_annotation)
     finder_chart.add_annotation(rotated_text_annotation)
@@ -46,14 +64,20 @@ def test_text_annotation_rotated(pivot, angle, fits_file, check_finder):
 @pytest.mark.parametrize("displacement", [(0, 0) * u.arcmin, (2.5, -4) * u.arcmin])
 def test_text_annotation_translated(displacement, fits_file, fits_center, check_finder):
     """Test translated text annotations."""
-    s = f"Some text"
+    s = "Some text"
     finder_chart = FinderChart(fits_file)
     text_annotation = TextAnnotation(
         fits_center, s, wcs=finder_chart.wcs, color="gray", horizontalalignment="left"
     )
     translated_text_annotation = text_annotation.translate(displacement)
     translated_text_annotation._kwargs["color"] = "blue"
-    legend = TextAnnotation(SkyCoord(ra="00h40m36s", dec="-59d55m30s"), f"Translated by {displacement.to_value(u.arcmin)} arcmin", wcs=finder_chart.wcs, color="blue", horizontalalignment="left")
+    legend = TextAnnotation(
+        SkyCoord(ra="00h40m36s", dec="-59d55m30s"),
+        f"Translated by {displacement.to_value(u.arcmin)} arcmin",
+        wcs=finder_chart.wcs,
+        color="blue",
+        horizontalalignment="left",
+    )
     finder_chart.add_annotation(text_annotation)
     finder_chart.add_annotation(translated_text_annotation)
     finder_chart.add_annotation(legend)
