6df21ca7a4aa957ec516579e17f3df2373f870ce
Christian Hettlage <hettlage@saao.ac.za>
2022-01-28 09:34:31 +0200

    Added missing __all__ member

--- src/imephu/annotation/general/__init__.py
+++ src/imephu/annotation/general/__init__.py
@@ -1,3 +1,4 @@
+from imephu.annotation.general.arrow import ArrowAnnotation
 from imephu.annotation.general.circle import CircleAnnotation
 from imephu.annotation.general.group import GroupAnnotation
 from imephu.annotation.general.line_path import LinePathAnnotation
@@ -5,6 +6,7 @@ from imephu.annotation.general.rectangle import RectangleAnnotation
 from imephu.annotation.general.text import TextAnnotation
 
 __all__ = [
+    "ArrowAnnotation",
     "CircleAnnotation",
     "GroupAnnotation",
     "LinePathAnnotation",
