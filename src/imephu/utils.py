928fe4f485529d1c39fa2d144d9af62c55f5b1e8
Christian Hettlage <hettlage@saao.ac.za>
2022-02-19 20:44:04 +0200

    Utility functions for mid positions and magnitude ranges for ephemerides

--- src/imephu/utils.py
+++ src/imephu/utils.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 from astropy.coordinates import SkyCoord
 
@@ -49,3 +49,60 @@ class Ephemeris:
         """Check that the epoch is timezone-aware."""
         if self.epoch.tzinfo is None or self.epoch.tzinfo.utcoffset(None) is None:
             raise ValueError("The epoch must be a timezone-aware datetime object.")
+
+
+def mid_position(start: SkyCoord, end: SkyCoord) -> SkyCoord:
+    """Return the mid position between a start and end position on the sky.
+
+    The mid position is the mid position on the great arc between the start and end
+    position.
+
+    Taken from https://github.com/astropy/astropy/issues/5766.
+
+    Parameters
+    ----------
+    start: `~astropy.coordinates.SkyCoords`
+        Start position on the sky.
+    end: `~astropy.coordinates.SkyCoords`
+        End position on the sky.
+    """
+    pa = start.position_angle(end)
+    separation = start.separation(end)
+    return start.directional_offset_by(pa, separation / 2)
+
+
+def ephemerides_magnitude_range(ephemerides: List[Ephemeris]) -> Optional[MagnitudeRange]:
+    """Return the magnitude range for a list of ephemerides.
+
+    The minimum (maximum) magnitude is the minimum (maximum) magnitude for all
+    ephemerides. If none of the ephemerides has a magnitude range, ``None`` is returned.
+
+    Parameters
+    ----------
+    ephemerides: list of `~imephu.utils.Ephemeris`
+        The list of ephemerides.
+
+    Returns
+    -------
+    `~imephu.utils.MagnitudeRange`, optional
+        The magnitude range for the list of ephemerides.
+    """
+    min_magnitude: Optional[float] = None
+    max_magnitude: Optional[float] = None
+    bandpass: Optional[str] = None
+    for ephemeris in ephemerides:
+        if ephemeris.magnitude_range:
+            if min_magnitude is None or ephemeris.magnitude_range.min_magnitude < min_magnitude:
+                min_magnitude = ephemeris.magnitude_range.min_magnitude
+            if max_magnitude is None or ephemeris.magnitude_range.max_magnitude > max_magnitude:
+                max_magnitude = ephemeris.magnitude_range.max_magnitude
+            if bandpass is None:
+                bandpass = ephemeris.magnitude_range.bandpass
+            elif ephemeris.magnitude_range.bandpass != bandpass:
+                raise ValueError("The bandpass must be the same for all ephemerides.")
+
+    if min_magnitude is not None:
+        return MagnitudeRange(min_magnitude=min_magnitude, max_magnitude=max_magnitude, bandpass=bandpass)
+    else:
+        return None
+
