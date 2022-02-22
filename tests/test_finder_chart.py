447d0c7a64e6fba130b5aa458edc58beda866961
hettlage <hettlage@saao.ac.za>
2022-02-22 10:06:14 +0200

    Non sidereal finder charts (#5)

--- tests/test_finder_chart.py
+++ tests/test_finder_chart.py
@@ -1,14 +1,35 @@
 import io
 import pathlib
+from contextlib import contextmanager
+from datetime import datetime, timedelta, timezone
+from typing import Any, cast
 from unittest import mock
 
 import numpy as np
 import pikepdf
 import pytest
 from astropy import units as u
+from astropy.coordinates import SkyCoord
 
+import imephu
 import imephu.service.survey
 from imephu.finder_chart import FinderChart
+from imephu.utils import Ephemeris, MagnitudeRange
+
+
+@contextmanager
+def _does_not_raise():
+    yield
+
+
+def _ephemeris(epoch, ra=0 * u.deg, dec=0 * u.deg):
+    return Ephemeris(
+        epoch=epoch,
+        position=SkyCoord(ra=ra, dec=dec),
+        magnitude_range=MagnitudeRange(
+            bandpass="V", min_magnitude=13, max_magnitude=13
+        ),
+    )
 
 
 def test_finder_chart_is_generated_from_path(check_finder):
@@ -71,3 +92,226 @@ def test_metadata_is_added_to_finder_chart_pdf(fits_file):
     meta = document.open_metadata()
     assert meta["dc:title"] == "Finder Chart"
     assert meta["xmp:CreatorTool"] == f"imephu {imephu.__version__}"
+
+
+def test_for_time_interval_start_must_be_timezone_aware():
+    """Test that the start time must be timezone-aware."""
+    start = datetime(2022, 2, 16, 15, 0, 0, 0)
+    end = datetime(2022, 2, 2, 10, 0, 0, 0, tzinfo=timezone.utc)
+    ephemerides = [
+        _ephemeris(datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc)),
+        _ephemeris(datetime(2022, 2, 2, 12, 0, 0, 0, tzinfo=timezone.utc)),
+    ]
+    with pytest.raises(ValueError) as excinfo:
+        g = FinderChart.for_time_interval(
+            start, end, ephemerides, 5 * u.arcmin, cast(Any, lambda x: 42)
+        )
+        next(g)
+    assert "start" in str(excinfo.value) and "timezone" in str(excinfo.value)
+
+
+def test_for_time_interval_end_must_be_timezone_aware():
+    """Test that the end time must be timezone-aware."""
+    start = datetime(2022, 2, 16, 15, 0, 0, 0, tzinfo=timezone.utc)
+    end = datetime(2022, 2, 2, 10, 0, 0, 0)
+    ephemerides = [
+        _ephemeris(datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc)),
+        _ephemeris(datetime(2022, 2, 2, 12, 0, 0, 0, tzinfo=timezone.utc)),
+    ]
+    with pytest.raises(ValueError) as excinfo:
+        g = FinderChart.for_time_interval(
+            start, end, ephemerides, 5 * u.arcmin, cast(Any, lambda x: 42)
+        )
+        next(g)
+    assert "end" in str(excinfo.value) and "timezone" in str(excinfo.value)
+
+
+@pytest.mark.parametrize(
+    "start,end,expectation",
+    [
+        (
+            datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc),
+            datetime(2022, 2, 16, 12, 0, 1, 0, tzinfo=timezone.utc),
+            _does_not_raise(),
+        ),
+        (
+            datetime(2022, 2, 16, 12, 0, 1, 0, tzinfo=timezone.utc),
+            datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc),
+            pytest.raises(ValueError),
+        ),
+        (
+            datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc),
+            datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc),
+            pytest.raises(ValueError),
+        ),
+    ],
+)
+def test_for_time_interval_start_must_be_earlier_than_end(start, end, expectation):
+    """Test that the start time must be earlier than the end time."""
+    ephemerides = [
+        _ephemeris(datetime(2022, 2, 15, 0, 0, 0, 0, tzinfo=timezone.utc)),
+        _ephemeris(datetime(2022, 2, 20, 12, 0, 0, 0, tzinfo=timezone.utc)),
+    ]
+    with expectation:
+        g = FinderChart.for_time_interval(
+            start, end, ephemerides, 5 * u.arcmin, cast(Any, lambda x: 42)
+        )
+        next(g)
+
+
+@pytest.mark.parametrize(
+    "start,end,expectation",
+    [
+        (
+            datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc),
+            datetime(2022, 2, 20, 12, 0, 0, 0, tzinfo=timezone.utc),
+            _does_not_raise(),
+        ),
+        (
+            datetime(2022, 2, 16, 11, 59, 59, 0, tzinfo=timezone.utc),
+            datetime(2022, 2, 20, 12, 0, 0, 0, tzinfo=timezone.utc),
+            pytest.raises(ValueError),
+        ),
+        (
+            datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc),
+            datetime(2022, 2, 20, 12, 0, 1, 0, tzinfo=timezone.utc),
+            pytest.raises(ValueError),
+        ),
+    ],
+)
+def test_for_time_interval_time_intervals_must_be_covered(start, end, expectation):
+    """Test that the time interval is covered by the ephemerides."""
+    ephemerides = [
+        _ephemeris(datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc)),
+        _ephemeris(datetime(2022, 2, 20, 12, 0, 0, 0, tzinfo=timezone.utc)),
+    ]
+    with expectation:
+        g = FinderChart.for_time_interval(
+            start, end, ephemerides, 5 * u.arcmin, cast(Any, lambda x: 42)
+        )
+        next(g)
+
+
+@pytest.mark.parametrize(
+    "max_track_length,expectation",
+    [
+        (-1 * u.arcmin, pytest.raises(ValueError)),
+        (0 * u.arcmin, pytest.raises(ValueError)),
+        (1 * u.arcmin, _does_not_raise()),
+    ],
+)
+def test_for_time_interval_max_track_length_must_be_positive(
+    max_track_length, expectation
+):
+    """Test that the maximum track length must be positive."""
+    start = datetime(2022, 2, 16, 15, 0, 0, 0, tzinfo=timezone.utc)
+    end = datetime(2022, 2, 20, 10, 0, 0, 0, tzinfo=timezone.utc)
+    ephemerides = [
+        _ephemeris(datetime(2022, 2, 16, 12, 0, 0, 0, tzinfo=timezone.utc)),
+        _ephemeris(datetime(2022, 2, 20, 12, 0, 0, 0, tzinfo=timezone.utc)),
+    ]
+    with expectation:
+        g = FinderChart.for_time_interval(
+            start, end, ephemerides, max_track_length, cast(Any, lambda x: 42)
+        )
+        next(g)
+
+
+@pytest.mark.parametrize(
+    "ras,decs",
+    [
+        # For a declination of 60 degrees the actual angle on the sky is half the
+        # difference in right ascension.
+        (
+            [0 * u.deg, 2.1 * u.deg, 4 * u.deg, 6 * u.deg],
+            [60 * u.deg, 60 * u.deg, 60 * u.deg, 60 * u.deg],
+        ),
+        (
+            [0 * u.deg, 2 * u.deg, 4.1 * u.deg, 6 * u.deg],
+            [60 * u.deg, 60 * u.deg, 60 * u.deg, 60 * u.deg],
+        ),
+        (
+            [0 * u.deg, 2 * u.deg, 4 * u.deg, 6.3 * u.deg],
+            [60 * u.deg, 60 * u.deg, 60 * u.deg, 60 * u.deg],
+        ),
+        (
+            [0 * u.deg, 0 * u.deg, 0 * u.deg, 0 * u.deg],
+            [0 * u.deg, 1.1 * u.deg, 2 * u.deg, 3 * u.deg],
+        ),
+        (
+            [0 * u.deg, 0 * u.deg, 0 * u.deg, 0 * u.deg],
+            [0 * u.deg, 1 * u.deg, 2.1 * u.deg, 3 * u.deg],
+        ),
+        (
+            [0 * u.deg, 0 * u.deg, 0 * u.deg, 0 * u.deg],
+            [0 * u.deg, 1 * u.deg, 2 * u.deg, 3.1 * u.deg],
+        ),
+    ],
+)
+def test_for_time_interval_requires_sufficiently_close_ephemeris_positions(ras, decs):
+    """Test that the maximum track length must not be exceeded on a finder chart."""
+    t = datetime(2022, 2, 17, 9, 0, 0, 0, tzinfo=timezone.utc)
+    ephemerides = [
+        _ephemeris(t + timedelta(hours=i), ra=ras[i], dec=decs[i])
+        for i, _ in enumerate(ras)
+    ]
+    start = ephemerides[0].epoch
+    end = ephemerides[-1].epoch
+    with pytest.raises(ValueError):
+        g = FinderChart.for_time_interval(
+            start, end, ephemerides, 1 * u.deg, cast(Any, lambda x: 42)
+        )
+        next(g)
+
+
+@pytest.mark.parametrize(
+    "positions,expected_indices",
+    [
+        # For a declination of 60 degrees the actual angle on the sky is half the
+        # difference in right ascension.
+        (
+            (
+                (0 * u.deg, 60 * u.deg),
+                (0.6 * u.deg, 60 * u.deg),
+                (1.8 * u.deg, 60 * u.deg),
+                (2.1 * u.deg, 60 * u.deg),
+                (3.9 * u.deg, 60 * u.deg),
+                (4 * u.deg, 60 * u.deg),
+            ),
+            ((0, 1, 2), (2, 3), (3, 4, 5)),
+        ),
+        (
+            (
+                (0 * u.deg, 0 * u.deg),
+                (0 * u.deg, 0.8 * u.deg),
+                (0 * u.deg, 1.2 * u.deg),
+                (0 * u.deg, 2.1 * u.deg),
+            ),
+            ((0, 1), (1, 2), (2, 3)),
+        ),
+    ],
+)
+def test_for_time_interval_creates_correct_finder_charts(positions, expected_indices):
+    """Test that the correct finder charts are created."""
+    t = datetime(2022, 2, 17, 0, 0, 0, 0, tzinfo=timezone.utc)
+    ephemerides = [
+        _ephemeris(
+            epoch=t + timedelta(hours=i), ra=positions[i][0], dec=positions[i][1]
+        )
+        for i, _ in enumerate(positions)
+    ]
+    expected_ephemerides = []
+    for index_group in expected_indices:
+        expected_ephemerides.append([ephemerides[i] for i in index_group])
+
+    start = t
+    end = t + timedelta(hours=len(positions) - 1)
+
+    # Fake function for creating a finder chart. It just returns the argument passed.
+    def fake_create_finder_chart(e):
+        return e
+
+    g = FinderChart.for_time_interval(
+        start, end, ephemerides, 1 * u.deg, cast(Any, fake_create_finder_chart)
+    )
+    assert [f for f in g] == expected_ephemerides
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,17 @@
 from contextlib import contextmanager
 from datetime import datetime, timezone
+from typing import Optional
 
 import pytest
+from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-from imephu.salt.finder_chart import MagnitudeRange
-from imephu.utils import Ephemeris
+from imephu.utils import (
+    Ephemeris,
+    MagnitudeRange,
+    ephemerides_magnitude_range,
+    mid_position,
+)
 
 
 @contextmanager
@@ -30,3 +36,104 @@ def test_ephemeris_epoch_must_be_timezone_aware(epoch, expectation):
                 min_magnitude=18, max_magnitude=18, bandpass="V"
             ),
         )
+
+
+@pytest.mark.parametrize(
+    "start,end,expected",
+    [
+        (
+            SkyCoord(ra=10 * u.deg, dec=0 * u.deg),
+            SkyCoord(ra=15 * u.deg, dec=0 * u.deg),
+            SkyCoord(ra=12.5 * u.deg, dec=0 * u.deg),
+        ),
+        (
+            SkyCoord(ra=130 * u.deg, dec=-20 * u.deg),
+            SkyCoord(ra=130 * u.deg, dec=-40 * u.deg),
+            SkyCoord(ra=130 * u.deg, dec=-30 * u.deg),
+        ),
+    ],
+)
+def test_mid_position_on_sky(start, end, expected):
+    """Test that the mid_position function returns the correct position."""
+    mp = mid_position(start=start, end=end)
+    assert mp.ra.to_value(u.deg) == pytest.approx(expected.ra.to_value(u.deg))
+    assert mp.dec.to_value(u.deg) == pytest.approx(expected.dec.to_value(u.deg))
+
+
+@pytest.mark.parametrize(
+    "bandpasses,expectation",
+    [
+        ((None, None), _does_not_raise()),
+        ((None, "V"), _does_not_raise()),
+        (("V",), _does_not_raise()),
+        (("R", "R"), _does_not_raise()),
+        (("V", "R", "V"), pytest.raises(ValueError)),
+    ],
+)
+def test_ephemerides_magnitude_range_requires_consistent_bandpass(
+    bandpasses, expectation
+):
+    """Test that the bandpass must be the same for every ephemeris."""
+    ephemerides = []
+    for bandpass in bandpasses:
+        if bandpass:
+            mr: Optional[MagnitudeRange] = MagnitudeRange(
+                min_magnitude=17.1, max_magnitude=17.2, bandpass=bandpass
+            )
+        else:
+            mr = None
+        ephemerides.append(
+            Ephemeris(
+                epoch=datetime(2022, 2, 19, 0, 0, 0, 0, tzinfo=timezone.utc),
+                position=SkyCoord(ra=0 * u.deg, dec=0 * u.deg),
+                magnitude_range=mr,
+            )
+        )
+
+    with expectation:
+        ephemerides_magnitude_range(ephemerides)
+
+
+@pytest.mark.parametrize(
+    "min_magnitudes,max_magnitudes,expected",
+    [
+        (
+            (16.9,),
+            (16.9,),
+            MagnitudeRange(min_magnitude=16.9, max_magnitude=16.9, bandpass="B"),
+        ),
+        (
+            (16.9,),
+            (17.0,),
+            MagnitudeRange(min_magnitude=16.9, max_magnitude=17.0, bandpass="B"),
+        ),
+        (
+            (12.5, None, 14.7, 11.3, 16.7, 11.5),
+            (12.7, None, 15, 11.3, 16.8, 11.5),
+            MagnitudeRange(min_magnitude=11.3, max_magnitude=16.8, bandpass="B"),
+        ),
+        ((None,), (None,), None),
+        ((None, None), (None, None), None),
+    ],
+)
+def test_ephemerides_magnitude_range(min_magnitudes, max_magnitudes, expected):
+    """Test that the ephemerides_magnitude_range function returns the correct range."""
+    ephemerides = []
+    for i, _ in enumerate(min_magnitudes):
+        if min_magnitudes[i] is not None:
+            mr: Optional[MagnitudeRange] = MagnitudeRange(
+                min_magnitude=min_magnitudes[i],
+                max_magnitude=max_magnitudes[i],
+                bandpass="B",
+            )
+        else:
+            mr = None
+        ephemerides.append(
+            Ephemeris(
+                epoch=datetime(2022, 2, 19, 0, 0, 0, 0, tzinfo=timezone.utc),
+                position=SkyCoord(ra=0 * u.deg, dec=0 * u.deg),
+                magnitude_range=mr,
+            )
+        )
+    magnitude_range = ephemerides_magnitude_range(ephemerides)
+    assert magnitude_range == expected
